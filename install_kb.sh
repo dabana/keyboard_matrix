7006031a86bc093a295abb5e83da51c96041a72a
dabana <david.banville2712@gmail.com>
2019-03-22 15:32:03 +0000

    add argument to isntall shell script.

--- install_kb.sh
+++ install_kb.sh
@@ -1,2 +1,2 @@
-dtc -W no-unit_address_vs_reg -I dts -O dtb -o 8x16matrix.dtbo 8x16matrix.dts
-cp ./8x16matrix.dtbo /boot/overlays/ 
+dtc -W no-unit_address_vs_reg -I dts -O dtb -o $1.dtbo $1.dts
+cp ./$1.dtbo /boot/overlays/ 
